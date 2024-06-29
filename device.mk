749501f1f151acb391ba80befe03dce5b902794c
Michael Bestas <mkbestas@lineageos.org>
2024-06-29 16:10:55 +0300

    bangkk: Update shipping API level
    Change-Id: Ifd581c4bb47d2ff552a4ab784eb8d53b7cb29aa3

--- device.mk
+++ device.mk
@@ -18,7 +18,8 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := 400dpi
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
-PRODUCT_SHIPPING_API_LEVEL := 31
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 33
 
 # Inherit from motorola sm6375-common
 $(call inherit-product, device/motorola/sm6375-common/common.mk)
