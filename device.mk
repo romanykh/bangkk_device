3965a69915ba6746304e96474b252cbd5f5350b8
Michael Bestas <mkbestas@lineageos.org>
2024-07-26 22:01:59 +0000

    bangkk: Build samsung NFC HAL
    Change-Id: Ie24e95fae238d8cd769b9cd555686991b699661c

--- device.mk
+++ device.mk
@@ -50,7 +50,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service.st \
+    android.hardware.nfc@1.2-service.samsung \
     com.android.nfc_extras \
     Tag
 
