44a1963359e98b59608496302b9fe0257bac9760
Aaron Kling <webgeek1234@gmail.com>
2024-08-19 16:43:32 -0500

    dubai: Set avb rollback indexes to match stock
    Using values from the version used for extract
    
    Change-Id: If91897f88bda061b2158fd92f9e02ef79929e232

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,5 +46,9 @@ TARGET_RECOVERY_UI_MARGIN_HEIGHT := 90
 BOOT_SECURITY_PATCH := 2024-11-01
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
+# Verified Boot
+BOARD_AVB_ROLLBACK_INDEX := 27
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := 27
+
 # inherit from the proprietary version
 include vendor/motorola/dubai/BoardConfigVendor.mk
