b50ed50c0bc13afbc2d4a0f97585faef7145faaa
ARYANGODxDOfficial <aryansanjay.080805@gmail.com>
2025-11-24 13:53:24 +0000

    bangkk: Bring up for Project Matrixx

--- device.mk
+++ device.mk
@@ -111,3 +111,8 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/bangkk/bangkk-vendor.mk)
+
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
